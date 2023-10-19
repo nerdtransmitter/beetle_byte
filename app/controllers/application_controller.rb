b4c467074399e8675bea1b713155c64e8e661137
Alessia Moison <adm2217@columbia.edu>
2023-10-19 18:35:06 +0200

    started pundit and some config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,39 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:home]
+  include Pundit::Authorization
 
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # Pundit: allow-list approach
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  # Uncomment when you *really understand* Pundit!
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
+
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :location, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password) }
 
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:username, :location, :email, :password, :current_password)
+      u.permit(:username, :email, :password, :current_password)
     end
   end
 end
