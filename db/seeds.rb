f9f49b24a4f3c924cf937e83b8d4bdfb3580c423
Alessia Moison <adm2217@columbia.edu>
2023-10-22 15:54:56 +0200

    added admin boolean to users and seeded 3 users with different roles, one as admin and PM, and first project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# USERS
+User.destroy_all
+puts "Creating users..."
+users = User.create!([
+  { first_name: "Alessia", last_name: "Moison", email: "adm2217@columbia.edu", password: "BeetleJuice96", password_confirmation: "BeetleJuice96",  role: "Project Manager", admin: true },
+  { first_name: "Roxie", last_name: "Bianchi", email: "roxieb@beetlebyte.com", password: "BeetleJuice96", password_confirmation: "BeetleJuice96", role: "Lead Developer", admin: false },
+  { first_name: "Tommy", last_name: "Shelby", email: "tommy@beetlebyte.com", password: "BeetleJuice96", password_confirmation: "BeetleJuice96", role: "Developer", admin: false }
+])
+
+
+puts "Users successfully created!"
+
+# PROJECTS
+# Find existing Project Manager user
+user = User.find_by(email: "adm2217@columbia.edu")
+
+# Create project with this user as a creator
+puts "Creating bug tracker project..."
+project = Project.create!(
+  title: "Beetle_Byte",
+  description: "A bug tracker for development teams.",
+  start_date: Date.new(2023, 10, 9),
+  target_date: Date.new(2023, 11, 20),
+  created_by_id: user.id
+)
+
+puts "First project created!"
