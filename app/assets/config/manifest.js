1a83005368a2f70b99e5db845bc4a45f860a6124
Alessia Moison <adm2217@columbia.edu>
2023-10-23 13:55:22 +0200

    pre-webpacker

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link popper.js
+//= link bootstrap.min.js
+//= link application.scss
+//= link manifest.json
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import 'bootstrap'
+import "application"
+import "@popperjs/core"
+import "@hotwired/turbo-rails"
+import "jquery"
+import "jquery_ujs"
+import "./jquery_ui"
