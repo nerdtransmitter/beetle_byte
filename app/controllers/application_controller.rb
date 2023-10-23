8617674375968a9f50e93a636cc24123e0ffef84
Alessia Moison <adm2217@columbia.edu>
2023-10-23 18:34:37 +0200

    modified permit params for user sign up and update and webpack config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,10 +30,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
 
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:username, :email, :password, :current_password)
+      u.permit(:first_name, :last_name, :email, :password, :current_password)
     end
   end
 end
