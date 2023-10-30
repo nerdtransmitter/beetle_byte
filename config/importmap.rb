aca8e5725a60c600ddc9af2655dcbf8833f36d1e
Alessia Moison <adm2217@columbia.edu>
2023-10-30 16:33:48 +0100

    importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,7 +8,6 @@ pin "application", preload: true
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js"
 pin "js-confetti", to: "https://ga.jspm.io/npm:js-confetti@0.11.0/dist/es/index.js"
-pin "@hotwired/turbo-rails", to: "turbo.js"
 
 # Custom files
 pin "confetti"
