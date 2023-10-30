ee2fe4dbcb6e8be8c3aa4f4eb020b3c65d35a110
Alessia Moison <adm2217@columbia.edu>
2023-10-30 17:42:19 +0100

    changed application.js to have the correct imports including removing turbo rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-import '@hotwire/turbo-rails'
-import 'bootstrap'
-import 'confetti'
+import 'bootstrap';
+
+// Import @popperjs/core
+import "@popperjs/core";
+
+// Import your custom confetti module (if it exists)
+import "confetti";
