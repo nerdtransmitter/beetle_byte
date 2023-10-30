822e084eeac500830ea6625b49b2efc3e9738702
Alessia Moison <adm2217@columbia.edu>
2023-10-30 16:47:37 +0100

    unpinned jspopper from importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,6 @@ pin "application", preload: true
 # Add new with `importmap pin package-name`
 # Ex : `importmap pin bootstrap`
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js"
 pin "js-confetti", to: "https://ga.jspm.io/npm:js-confetti@0.11.0/dist/es/index.js"
 
 # Custom files
