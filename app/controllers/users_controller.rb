bacddfbc0460dd16ca422d9575045a9393ef75b5
Alessia Moison <adm2217@columbia.edu>
2023-10-31 11:46:49 +0100

    users controller generated, user policies check for if user is admin to authorize updating a users role and allows user to update their profiles other than roles

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  # Only admins can update and remove roles.
+  # But users can see their own profile and other users' profiles.
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def index
+    @users = policy_scope(User)
+    authorize @users
+  end
+
+  def update
+    @user = User.find(params[:id])
+    authorize @user
+
+    if @user.update(user_params)
+      redirect_to @user, notice: "User updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(policy(@user).permitted_attributes)
+  end
+end
