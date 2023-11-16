0613d82f62d361b860ea967c85e06af3d4b10486
Alessia Moison <adm2217@columbia.edu>
2023-11-16 16:17:43 +0100

    fixed sign out not working by switching turbo from importmaps to gem

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,10 @@
 import 'bootstrap';
-// import "@hotwire/turbo-rails";
 import "@popperjs/core";
+
+// import "@hotwire/turbo-rails";
+// import "@hotwire/turbo-rails";
+// console.log("Hello from application.js");
+
 // Import your custom confetti module (if it exists)
 import "confetti";
+import "@hotwired/turbo-rails"
