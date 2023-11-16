bb5a8390d22e88abfbfac5660a58d06acea61485
Alessia Moison <adm2217@columbia.edu>
2023-11-16 17:11:54 +0100

    edited navbar, reinstated employees routes and fixed projects controller with correct pundit authorization to solve issue with creating a new project

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,6 @@
 import 'bootstrap';
 import "@popperjs/core";
 
-// import "@hotwire/turbo-rails";
-// import "@hotwire/turbo-rails";
-// console.log("Hello from application.js");
-
 // Import your custom confetti module (if it exists)
 import "confetti";
 import "@hotwired/turbo-rails"
