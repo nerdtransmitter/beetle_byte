05f696516a9fb5ae1fe40fe726686a093b853d6d
Alessia Moison <adm2217@columbia.edu>
2023-11-23 13:06:46 +0100

    added to ticket policies and refactored scope

--- app/policies/ticket_policy.rb
+++ app/policies/ticket_policy.rb
@@ -1,24 +1,31 @@
 class TicketPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      if user.admin?
-        # Admins can see all tickets
-        scope.all
-      elsif user_lead_dev_projects.any?
+      return scope.all if user.admin? || user.role == 0
+
+      if user_lead_dev_projects.any?
         # Users with lead_dev role can see tickets in their lead_dev projects
         scope.where(project_id: user_lead_dev_projects)
       else
         # Regular users can see only their own tickets
-        scope.where('created_by_id = ? OR dev_id = ?', user.id, user.id)
+        user_tickets
       end
     end
 
     private
 
+    def user_tickets
+      scope.where('created_by_id = :user_id OR dev_id = :user_id', user_id: user.id)
+    end
+
     def user_lead_dev_projects
       # Retrieve the IDs of projects where the user is the lead developer
       Project.where(lead_dev_id: user.id).pluck(:id)
     end
+  end
+
+  def show?
+    user.admin? || user.id == ticket.dev_id || user.id == ticket.project.lead_dev_id || user.role == 0
   end
 
   def create?
@@ -33,16 +40,19 @@ class TicketPolicy < ApplicationPolicy
 
   def update?
     # Only the assigned developer, or the project lead, or admin can edit ticket details
-    user.admin? || user.id == ticket.dev_id || user.id == ticket.project.lead_dev_id
+    user.admin? || user.id == ticket.dev_id || user.id == ticket.project.lead_dev_id || user.id == ticket.project.manager_id
   end
 
   def change_status?
-    # If a developer resolves the ticket, they should be able to change status
-    user.admin? || user.id == ticket.dev_id || user.id == ticket.project.lead_dev_id
+    user.admin? || user.id == ticket.dev_id || user.id == ticket.project.lead_dev_id || user.id == ticket.project.manager_id
   end
 
   def destroy?
-    user.admin? || user.role == 0 || user.role == 1
+    user.admin? || user.role == 0 || ticket.created_by_id == user.id
+  end
+
+  def close_ticket?
+    user.admin? || user.id == ticket.dev_id || user.id == ticket.project.lead_dev_id || user.id == ticket.project.manager_id
   end
 
   private
