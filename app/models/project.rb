07b0f1ca7f7e4f91241987eb380f6f4cb184a55e
Alessia Moison <adm2217@columbia.edu>
2023-11-23 15:23:33 +0100

    Similarly reorganized models for ticket and project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ApplicationRecord
+  # VALIDATIONS
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+
+  # ASSOCIATIONS
   belongs_to :created_by, class_name: 'User', foreign_key: 'created_by_id'
   belongs_to :updated_by, class_name: 'User', foreign_key: 'updated_by_id', optional: true
   belongs_to :lead_dev, class_name: 'User', foreign_key: 'lead_dev_id', optional: true
@@ -6,6 +11,4 @@ class Project < ApplicationRecord
 
   has_many :tickets, dependent: :destroy
 
-  validates :title, presence: true, uniqueness: true
-  validates :description, presence: true
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,21 +1,23 @@
 class Ticket < ApplicationRecord
   before_save :set_default_status, if: -> { status.nil? }
 
+  # ENUMS
+  enum status: { open: 0, in_progress: 1, in_qa: 2, ready_for_release: 3, closed: 4, reopened: 5 }
+  enum priority: { low: 1, medium: 2, high: 3, critical: 4 }
+
+  # VALIDATIONS
+  validates :summary, presence: true
+  validates :project, presence: true
+  validates :status, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :priority, inclusion: { in: [nil, 1, 2, 3, 4] }
+
+  # ASSOCIATIONS
   belongs_to :project
   belongs_to :created_by, class_name: 'User', foreign_key: 'created_by_id'
   belongs_to :modified_by, class_name: 'User', foreign_key: 'modified_by_id', optional: true
   belongs_to :dev, class_name: 'User', foreign_key: 'dev_id', optional: true
 
-  validates :summary, presence: true
-  validates :project, presence: true
-
-  # Mapping integer values to status names
-  enum status: { open: 0, in_progress: 1, pending: 2, closed: 3, rejected: 4, reopened: 5 }
-  validates :status, inclusion: { in: [0, 1, 2, 3, 4, 5] }
-  # Mapping integer values to priority names
-  enum priority: { unassigned: nil, critical: 1, high: 2, medium: 3, low: 4 }
-  validates :priority, inclusion: { in: [nil, 1, 2, 3, 4] }
-
+  # CALLBACKS
   # Set default status to 'Open' if none given
   def set_default_status
     self.status = 'Open'
