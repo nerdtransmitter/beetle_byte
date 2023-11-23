83ecc48beb2d677af65bcf3fb5962b3dc034408c
Alessia Moison <adm2217@columbia.edu>
2023-11-23 15:19:32 +0100

    Modified seed and added tickets to it using correct values based on enums in addition to editing the validations and organisation of user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # ENUMS
+  enum role: { project_manager: 0, lead_developer: 1, developer: 2 }
+
+  # VALIDATIONS
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:password_digest] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:password_digest] }
+  validates :admin, presence: true
+  validates :role, inclusion: { in: roles.keys }
+
+
+  # ASSOCIATIONS
   has_many :projects
   has_many :created_tickets, class_name: 'Ticket', foreign_key: 'created_by_id'
   has_many :modified_tickets, class_name: 'Ticket', foreign_key: 'modified_by_id'
   has_many :dev_tickets, class_name: 'Ticket', foreign_key: 'dev_id'
   # has_one_attached :photo
 
-  validates :first_name, :last_name, :email, presence: true
-  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:password_digest] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:password_digest] }
-  validates :admin, presence: true
-
-  # USER ROLES
-  enum role: { project_manager: 0, lead_developer: 1, developer: 2 }
-  validates :role, inclusion: { in: [0, 1, 2] }
-
+  # CALLBACKS
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
   end
@@ -28,7 +32,7 @@ class User < ApplicationRecord
     role.to_s.humanize.titleize
   end
 
-  # Helper method to encapsulate the admin or a specific role check
+  # Helper method to encapsulate the admin or a specific role check used in Pundit policies
   def has_role_or_admin?(role_name)
     admin? || self.send("#{role_name}?")
   end
