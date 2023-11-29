6410042dbe43802570cd43e55179a91a28e483db
Alessia Moison <adm2217@columbia.edu>
2023-11-29 22:19:41 +0100

    Redid employees index table with linking to profile, dark mode and avatars

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -16,6 +16,7 @@ class Ticket < ApplicationRecord
   belongs_to :created_by, class_name: 'User', foreign_key: 'created_by_id'
   belongs_to :modified_by, class_name: 'User', foreign_key: 'modified_by_id', optional: true
   belongs_to :dev, class_name: 'User', foreign_key: 'dev_id', optional: true
+  # has_many_attached :attachments
 
   # CALLBACKS
   # Set default status to 'Open' if none given
